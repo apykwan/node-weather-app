e3ad9b38cbcf9825282271450b8ab93d56916b81
apkwan <apkwan317@gmail.com>
2020-04-23 21:39:49 -0700

    Update PORT

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 // console.log(__dirname);
 console.log(path.join(__dirname, '../public/'));
